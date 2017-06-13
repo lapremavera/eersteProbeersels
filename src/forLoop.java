9b1b608ebfa9fb694e6fc55c2c9fc18ed5678118
Prema Van den Broecke <lapremavera@gmail.com>
2017-06-13 14:58:46 +0200

    update uit de les

---
+++ src/forLoop.java
@@ -0,0 +1,16 @@
+public class forLoop {
+
+    public static void main(String[] args) {
+
+        int getal = 0;
+        while (getal < 10) {
+
+            System.out.println("Getal is " + getal);
+            getal++;
+        }
+
+        for (int i = 0; i< 10; i++){
+            System.out.println("Getal is "+ getal);
+        }
+    }
+}
---
+++ src/loop.java
@@ -0,0 +1,19 @@
+public class loop{
+    public static void main (String []args){
+
+        int getal = 0;
+        while (getal < 10) {
+
+            System.out.println( "Getal is " + getal);
+            getal = getal + 1;
+
+            /*volgende formules kunnen ook
+            getal += 1;  // getal met wat er na staat optellen
+            getal ++; // getal met 1 optellen
+
+            bestaat ook met -
+            */
+
+        }
+    }
+}
